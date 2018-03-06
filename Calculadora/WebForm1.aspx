19658e05cb98f58cfe2361353e578652d9918d21
Luiyi Plaza <joseplaza@unicomfacauca.edu.co>
2018-03-06 14:52:57 -0500

    Ejercicio Parcial Calculadora

---
+++ Calculadora/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Calcu.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Calculadora</title>
+    <style type="text/css">
+        #form1 {
+            width: 209px;
+        }
+    </style>
+</head>
+<body style="margin-left: 78px">
+    <form id="form1" runat="server">
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem>Sumar</asp:ListItem>
+                <asp:ListItem>Restar</asp:ListItem>
+                <asp:ListItem>Multiplicar</asp:ListItem>
+                <asp:ListItem>Dividir</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <p>
+            Valor1<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Valor2<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p style="width: 98px">
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calcular" />
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Resultado"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server" style="margin-bottom: 0px"></asp:TextBox>
+        </p>
+        <p style="height: 26px; width: 1776px">
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Limpiar" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Calculadora/WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calcu
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (DropDownList1.Text == "Sumar")
+
+            {
+                TextBox3.Text = Convert.ToString(Convert.ToDouble(TextBox1.Text) + Convert.ToDouble(TextBox2.Text));
+            }
+
+            if (DropDownList1.Text == "Restar")
+
+            {
+                TextBox3.Text = Convert.ToString(Convert.ToDouble(TextBox1.Text) - Convert.ToDouble(TextBox2.Text));
+            }
+
+            if (DropDownList1.Text == "Multiplicar")
+
+            {
+                TextBox3.Text = Convert.ToString(Convert.ToDouble(TextBox1.Text) * Convert.ToDouble(TextBox2.Text));
+            }
+
+            if (DropDownList1.Text == "Dividir")
+            {
+                if (TextBox2.Text.Equals("0"))
+                {
+                    TextBox3.Text = "Valor no aceptado";
+                }
+                else
+                {
+                    TextBox3.Text = Convert.ToString(Convert.ToDouble(TextBox1.Text) / Convert.ToDouble(TextBox2.Text));
+
+                }
+                
+
+            }
+
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+        }
+    }
+}
